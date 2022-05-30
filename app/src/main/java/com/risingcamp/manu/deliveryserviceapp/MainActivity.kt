0b0267ae211846a7f8b4bb78e51c9e778fc471de
SONG JI HYUK <jihy1325@gmail.com>
2022-05-30 18:17:51 +0900

    Initial commit

---
+++ app/src/main/java/com/risingcamp/manu/deliveryserviceapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.risingcamp.manu.deliveryserviceapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.risingcamp.manu.deliveryserviceapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Glide.with(this).load(R.raw.main_delivery_move).override(180).into(binding.bm1GifImg)
+    }
+}
